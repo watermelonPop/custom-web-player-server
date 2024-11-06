e6e08480d350c836de8d5b1a21c7cebce446f4de
watermelonPop <SK21007@Tamu.edu>
2024-11-06 16:39:16 -0600

    fix

--- server/index.js
+++ server/index.js
@@ -66,7 +66,7 @@ app.get('/auth/callback', (req, res) => {
   request.post(authOptions, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       access_token = body.access_token;
-      res.redirect('https://spotify-web-player-seven.vercel.app/');
+      res.redirect('http://localhost:3000');
     }
   });
 });
